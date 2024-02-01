97b664a669209ec70b6b822af6332ba92b7f0d5d
Murad Bashirli <murad.bashirli@pashabank.az>
2024-02-01 01:09:31 -0500

    Order-Service: Update functions

--- order-service/src/main/java/dp/ms/orderservice/controller/OrderController.java
+++ order-service/src/main/java/dp/ms/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package dp.ms.orderservice.controller;
 import dp.ms.orderservice.dto.OrderRequest;
 import dp.ms.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
@@ -20,6 +22,7 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest){
+        log.info("Place order request received");
         orderService.placeOrder(orderRequest);
         return "Order placed successfully";
     }
--- order-service/src/main/java/dp/ms/orderservice/service/OrderService.java
+++ order-service/src/main/java/dp/ms/orderservice/service/OrderService.java
@@ -1,5 +1,7 @@
 package dp.ms.orderservice.service;
 
+import dp.ms.orderservice.client.InventoryClient;
+import dp.ms.orderservice.dto.InventoryResponse;
 import dp.ms.orderservice.dto.OrderLineItemsDto;
 import dp.ms.orderservice.dto.OrderRequest;
 import dp.ms.orderservice.model.Order;
@@ -9,16 +11,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class OrderService {
 
+    private final InventoryClient inventoryClient;
     private final OrderRepository orderRepository;
     public void placeOrder(OrderRequest orderRequest){
+
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -28,7 +34,20 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).collect(Collectors.toList());
+
+        // Call Inventory Service, Place order if Item is in Stock
+        InventoryResponse[] inventoryResponses = inventoryClient.isInStock(skuCodes);
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponses).allMatch(InventoryResponse::getIsInStock);
+
+        if (Boolean.TRUE.equals(allProductsInStock)){
+            orderRepository.save(order);
+        }
+        else {
+            throw new IllegalArgumentException("Product not in stock. Please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
