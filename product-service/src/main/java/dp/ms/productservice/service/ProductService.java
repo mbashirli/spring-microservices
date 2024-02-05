d3c485ec45a96a87881bc322d95544ea8ed823e5
Murad Bashirli <mbashirlee@gmail.com>
2024-02-05 16:38:19 -0500

    Add logging to the service class

--- product-service/src/main/java/dp/ms/productservice/service/ProductService.java
+++ product-service/src/main/java/dp/ms/productservice/service/ProductService.java
@@ -44,6 +44,7 @@ public class ProductService {
     }
 
     public void deleteProduct(String productId) {
+        log.info("Delete product method called");
         if (!productRepository.existsById(productId)) {
             throw new ProductNotFoundException(productId.toString());
         }
@@ -51,6 +52,7 @@ public class ProductService {
     }
 
     public ProductDTO updateProduct(String productId, ProductDTO productDTO) {
+        log.info("Update product method called");
         AtomicReference<Product> atomicReference = new AtomicReference<>();
         productRepository.findById(productId).ifPresentOrElse(product -> {
             product.setDescription(productDTO.getDescription());
@@ -64,11 +66,13 @@ public class ProductService {
     }
 
     public ProductDTO getProductById(String productId) {
+        log.info("Get product by id method called");
         return productRepository.findById(productId).map(productMapper::productToProductDTO)
                 .orElseThrow(() -> new ProductNotFoundException(productId));
     }
 
     public ProductDTO patchProduct(String productId, ProductDTO productDTO) {
+        log.info("Patch product method called");
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ProductNotFoundException(productId));
 
