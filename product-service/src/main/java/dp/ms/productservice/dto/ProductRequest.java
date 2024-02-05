36e0f2b96b58c01790d1b09658d3014dbd4e3fdf
Murad Bashirli <mbashirlee@gmail.com>
2024-02-05 16:33:56 -0500

    Change UUID id back to String id

--- product-service/src/main/java/dp/ms/productservice/dto/ProductRequest.java
+++ product-service/src/main/java/dp/ms/productservice/dto/ProductRequest.java
@@ -1,5 +1,6 @@
 package dp.ms.productservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,14 +14,18 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "DTO for ProductRequest")
 public class ProductRequest {
 
+    @Schema(description = "Name")
     @NotBlank(message = "name may not be blank")
     private String name;
 
+    @Schema(description = "Description")
     @NotBlank(message = "description may not be blank")
     private String description;
 
+    @Schema(description = "Price")
     @NotNull(message = "price may not be blank")
     private BigDecimal price;
 }
